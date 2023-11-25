021615a29f96371d396dbfe546e2d9db29d2d315
churtadov <carohurtado2010@hotmail.com>
2023-11-25 00:34:03 -0600

    Primer commit

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        //El simbolo ? se utiliza para indicar que el dato puede ir nulo
+        public string? Title { get; set; }
+        //Las etiquetas se utilizan para dar un formato especifico al tipo de dato que se solicita
+        [DataType(DataType.Date)]
+        public DateTime RealeseDate { get; set; }
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageMovieContext>>()))
+            {
+                //En caso de conexto nulo, se envia un mensaje de error
+                if (context == null || context.Movie == null) 
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                
+                }
+
+                if (context.Movie.Any())
+                {
+                    //Base de datos muestra todo lo que contiene esta clase
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Spiderman",
+                        RealeseDate = DateTime.Parse("1991-02-01"),
+                        Genre = "Aventura",
+                        Price = 55.96M,
+                    },
+                    new Movie
+                    {
+                        Title = "Spiderman II",
+                        RealeseDate = DateTime.Parse("1999-10-11"),
+                        Genre = "Comic",
+                        Price = 45M,
+                    },
+                    new Movie
+                    {
+                        Title = "Spiderman III",
+                        RealeseDate = DateTime.Parse("2023-10-15"),
+                        Genre = "Caricatura",
+                        Price = 6.55M,
+                    }
+
+                    );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
